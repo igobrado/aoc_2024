a442cc3d94568e16305f3af8cb35bf8b8327ee00
Igor Obradovic <igor.obradovic@cetitec.com>
2024-12-09 11:22:55 +0100

    Add common build for all days

---
+++ 09/part_1/main.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <ranges>
+#include <set>
+#include <string>
+#include <vector>
+
+template<typename IteratorType>
+struct custom_range
+{
+    custom_range(IteratorType begin, IteratorType end = {})
+    : m_begin{begin}
+    , m_end{end}
+    {
+    }
+
+    auto begin() -> decltype(auto)
+    {
+        return m_begin;
+    }
+    auto end() -> decltype(auto)
+    {
+        return m_end;
+    }
+
+    IteratorType m_begin;
+    IteratorType m_end;
+};
+
+int main(int argc, char** argv)
+{
+    if (argc < 1)
+    {
+        std::puts("Input files needed");
+        return -1;
+    }
+    std::puts(std::format("Using file on path: {}", argv[1]).c_str());
+
+    std::ifstream stream{argv[1]};
+    std::puts(std::format("TodoImplementMe:").c_str());
+}
